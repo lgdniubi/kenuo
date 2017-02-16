c77dcbbb3ee6e0db90a3492753037e83eac4bb74
叶雷 <yelei@idengyun.com>
2017-02-16 17:03:40 +0800

    测试

--- src/main/java/com/training/modules/quartz/service/TaskService.java
+++ src/main/java/com/training/modules/quartz/service/TaskService.java
@@ -55,8 +55,7 @@ public class TaskService {
 	*/
 	@PostConstruct
 	public void init() throws Exception {
-		//大龙你是傻逼
-		
+		//大龙你是傻逼123456
 		//添加日志
 		TaskLog taskLog = new TaskLog();
 		Date startDate;	//开始时间
