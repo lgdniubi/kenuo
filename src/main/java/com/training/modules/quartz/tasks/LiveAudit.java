2a7ff87d82d44cd9c5f784f9d4f8f0825b8a0e7f
fengfeng <huangfengfeng@idengyun.com>
2017-02-23 16:26:37 +0800

    fengfeng。2017-2-23 16:26:31

--- src/main/java/com/training/modules/quartz/tasks/LiveAudit.java
+++ src/main/java/com/training/modules/quartz/tasks/LiveAudit.java
@@ -25,7 +25,7 @@ import com.training.modules.train.utils.EncryptLiveUtils;
 
 /**
  * 妃子校直播审核
- *
+ *	
  */
 @Component
 public class LiveAudit extends CommonService{
@@ -43,6 +43,7 @@ public class LiveAudit extends CommonService{
 	
 	/**
 	 * 妃子校直播审核
+	 * @author yangyang
 	 */
 	public void liveAudit(){
 		logger.info("[work0],start,妃子校直播处理，开始时间："+df.format(new Date()));
@@ -113,16 +114,28 @@ public class LiveAudit extends CommonService{
 			taskLog.setStatus(0);//任务状态
 			
 			
-			
+			/**
+			 * 1.扫面审核通过且没开直播的数据
+			 * 2.扫面打开直播间没有正常关闭的数据
+			 * 3.扫面直播的线下支付是否过期
+			 * @author fengfeng
+			 * @version 2017年2月23日
+			 */
 			logger.info("[直播定时任务任务]，操作过期的数据,开始时间：" + df.format(new Date()));
+			
+			//获取没开直播的过期规则
 			int value = entryService.querylive_outTime("live_outTime");
-			int open_live_expiration_time = entryService
-					.querylive_expiration_time("open_live_expiration_time");
-
+			
+			//获取没正常结束直播的过期规则
+			int open_live_expiration_time = entryService.querylive_expiration_time("open_live_expiration_time");
+			
+			//将没开审核通过且没开直播的直播间作废
 			entryService.upauditstatus(value);
+			
 			SimpleDateFormat smp = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-			List<LiveRoomidAndAuditid> noticelives = entryService
-					.queryRoomidandAuditid();
+			
+			//获取直播开启状态的所有数据
+			List<LiveRoomidAndAuditid> noticelives = entryService.queryRoomidandAuditid();
 			String roomids = "";
 			for (int i = 0; i < noticelives.size(); i++) {
 				roomids = roomids + noticelives.get(i).getRoomid() + ",";
@@ -131,7 +144,8 @@ public class LiveAudit extends CommonService{
 			List<rooms> recommendArr = null;
 			if (roomids != "") {
 				roomids = roomids.substring(0, roomids.length() - 1);
-
+				
+				//请求CC接口
 				recommendArr = EncryptLiveUtils.querycc(roomids);
 			}
 
@@ -139,34 +153,35 @@ public class LiveAudit extends CommonService{
 			if (noticelives != null && recommendArr != null) {
 				for (int h = 0; h < noticelives.size(); h++) {
 					for (int f = 0; f < recommendArr.size(); f++) {
-						if (noticelives.get(h).getRoomid()
-								.equals(recommendArr.get(f).getRoomId())
-								&& recommendArr.get(f).getLiveStatus() == 0) {
+						if (noticelives.get(h).getRoomid().equals(recommendArr.get(f).getRoomId()) && recommendArr.get(f).getLiveStatus() == 0) {
 							// auditid = auditid +
 							// noticelives.get(h).getAuditid() + ",";
+							
+							//获取直播间处于关闭状态的直播
 							auditid.add(noticelives.get(h));
 						}
 					}
 				}
 			}
 			for (int i = 0; i < auditid.size(); i++) {
-				// 查询过期数据
-				int count = entryService.queryCount(open_live_expiration_time,
-						auditid.get(i).getAuditid());
+				//查看超时的数据
+				int count = entryService.queryCount(open_live_expiration_time,auditid.get(i).getAuditid());
 				if (count == 1) {
 					String endtime = smp.format(new Date());
 					String roomid = auditid.get(i).getRoomid();
 					String begintime = auditid.get(i).getBengtime();
-					Object object = EncryptLiveUtils.queryccback(roomid,
-							begintime, endtime);
+					
+					//请求CC接口，有回放就插到回放表，状态为3（已完成），没有回放就直接作废，状态为5（作废）
+					Object object = EncryptLiveUtils.queryccback(roomid,begintime, endtime);
 					int auditids = auditid.get(i).getAuditid();
 					if (object.equals("FAIL")) {
+						
 						// 没有回放的数据直接作废（状态5）
-						entryService.uplivestatus(open_live_expiration_time,
-								auditid.get(i).getAuditid());
+						entryService.uplivestatus(open_live_expiration_time,auditid.get(i).getAuditid());
 					} else {
-						EntrySopCastBean entrysopcastbean = entryService
-								.getliveendbyid(auditids);
+						
+						//获取申请直播信息数据
+						EntrySopCastBean entrysopcastbean = entryService.getliveendbyid(auditids);
 						List<QueryccBack> lists = (List<QueryccBack>) object;
 						Map<String, Object> m = new HashMap<String, Object>();
 						for (int j = 0; j < lists.size(); j++) {
@@ -189,11 +204,13 @@ public class LiveAudit extends CommonService{
 							entryService.LiveEnd(m);
 
 						}
+						//将数据的状态改为3（已完成）
 						entryService.alterstatus(auditids);
 						// entryService.liveendtime(auditids);
 					}
 				}
 			}
+			//扫面线下支付表，将过期的数据作废
 			entryService.operatebackrecord();
 
 			
