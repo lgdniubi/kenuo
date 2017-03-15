7efe777ec38783682bb03c6263cb78ee10355e4a
叶雷 <yelei@idengyun.com>
2017-03-15 20:50:27 +0800

    优化订单的导出：新增导出省市县

--- src/main/java/com/training/modules/ec/entity/Orders.java
+++ src/main/java/com/training/modules/ec/entity/Orders.java
@@ -24,7 +24,7 @@ public class Orders extends TreeEntity<Orders> {
 	private String consignee;			//收货人
 //	private String country;				//国家
 //	private String province;			//省份
-	private	 String city;				//城市
+	private	String city;				//城市
 	private String cityname;			//城市名称
 //	private	 String district;			//县区s
 //	private String twonl;				//城镇
@@ -169,6 +169,12 @@ public class Orders extends TreeEntity<Orders> {
 	private String strReal;				//导出 字段 订单类型
 	private String strChannel;		//订单创建类型
 	private String strAddTime;		//导出字段 下单时间
+	
+	private String newProvince;     //导出字段  省
+	private String newCity;         //导出字段  市
+	private String newDistrict;     //导出字段  县
+	
+	private String oldAddress;      //修改前的地址
 	
 	public String getSearchIsReal() {
 		return searchIsReal;
@@ -1440,6 +1446,40 @@ public class Orders extends TreeEntity<Orders> {
 	public void setNewShippingPrice(String newShippingPrice) {
 		this.newShippingPrice = newShippingPrice;
 	}
+	
+	@JsonIgnore
+	@ExcelField(title="省", align=2, sort=17)
+	public String getNewProvince() {
+		return newProvince;
+	}
+	public void setNewProvince(String newProvince) {
+		this.newProvince = newProvince;
+	}
+	
+	@JsonIgnore
+	@ExcelField(title="市", align=2, sort=18)
+	public String getNewCity() {
+		return newCity;
+	}
+	public void setNewCity(String newCity) {
+		this.newCity = newCity;
+	}
+	
+	@JsonIgnore
+	@ExcelField(title="县", align=2, sort=19)
+	public String getNewDistrict() {
+		return newDistrict;
+	}
+	public void setNewDistrict(String newDistrict) {
+		this.newDistrict = newDistrict;
+	}
+	public String getOldAddress() {
+		return oldAddress;
+	}
+	public void setOldAddress(String oldAddress) {
+		this.oldAddress = oldAddress;
+	}
+	
 	
 	
 	
