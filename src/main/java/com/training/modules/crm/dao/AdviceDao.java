79f5e5383eb389c9d9937e2102dbaf8d12049742
李兴 <lixing@idengyun.com>
2017-03-31 09:23:32 +0800

    crm投资咨询模块

---
+++ src/main/java/com/training/modules/crm/dao/AdviceDao.java
@@ -0,0 +1,51 @@
+package com.training.modules.crm.dao;
+
+import java.util.List;
+
+import com.training.common.persistence.CrudDao;
+import com.training.common.persistence.annotation.MyBatisDao;
+import com.training.modules.crm.entity.Complain;
+import com.training.modules.sys.entity.User;
+
+/**
+ * @author：星星
+ * @description：投诉咨询dao类 
+ * 2017年3月10日
+ */
+@MyBatisDao
+public interface AdviceDao extends CrudDao<Complain>{
+
+	public int saveQuestion(Complain complain);
+	
+	public void saveSolve(Complain complain);
+	
+	public Complain detailed(Complain complain);
+	
+	public List<Complain> procedure(Complain complain);
+
+	public Complain selectHandle(Complain complain);
+
+	public void creatHandle(Complain complains);
+	
+	public void saveHandle(Complain complain);
+
+	public Complain selectMember(Complain complain);
+
+	public List<Complain> selectStatus(Complain complain);
+
+	public void creatResult(Complain complain);
+
+	public Complain selectId(Complain complain);
+
+	public List<User> findUser(User user);
+
+	public Complain getUser(String mobile);
+
+	public List<Complain> getRedirectUserId(Complain complain);
+
+	public int getCount(Complain complain);
+
+	public List<Complain> selectSeek(Complain complain);
+
+
+}
---
+++ src/main/java/com/training/modules/crm/service/AdviceService.java
@@ -0,0 +1,169 @@
+package com.training.modules.crm.service;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import com.training.common.service.CrudService;
+import com.training.modules.crm.dao.AdviceDao;
+import com.training.modules.crm.entity.Complain;
+import com.training.modules.sys.entity.User;
+
+/**
+ * @author：星星
+ * @description：投诉咨询service类 
+ * 2017年3月10日
+ */ 
+@Service
+public class AdviceService extends CrudService<AdviceDao, Complain>{
+	
+	/**
+	 * @author：星星
+	 * @description：保存问题
+	 * 2017年3月17日
+	 */
+	@Transactional(readOnly = false)
+	public int saveQuestion(Complain complain){
+		int id = dao.saveQuestion(complain);
+		return id;
+	}
+	
+	/**
+	 * @author：星星
+	 * @description：保存处理过程
+	 * 2017年3月17日
+	 */
+	@Transactional(readOnly = false)
+	public void saveSolve(Complain complain){
+       dao.saveSolve(complain);		
+	}
+
+	/**
+	 * @author：星星
+	 * @description：问题详情查询
+	 * 2017年3月17日
+	 */
+	public Complain detailed(Complain complain) {
+		return dao.detailed(complain);
+	}
+
+	/**
+	 * @author：星星
+	 * @description：获取一个问题的所有处理过程
+	 * 2017年3月17日
+	 */
+	public List<Complain> procedure(Complain complain){
+		return dao.procedure(complain);
+	}
+
+	/**
+	 * @author：星星
+	 * @description：查询最后一次处理过程
+	 * 2017年3月17日
+	 */
+	public Complain selectHandle(Complain complain) {		
+		return dao.selectHandle(complain);		
+	}
+
+	/**
+	 * @author：星星
+	 * @description：改变问题的状态
+	 * 2017年3月17日
+	 */
+	@Transactional(readOnly = false)
+	public void creatHandle(Complain complains) {
+		 dao.creatHandle(complains);		
+	}
+	
+	/**
+	 * @author：星星
+	 * @description：保存处理过程
+	 * 2017年3月17日
+	 */
+	@Transactional(readOnly = false)
+	public void saveHandle(Complain complain) {
+		dao.saveHandle(complain);
+	}
+
+	/**
+	 * @author：星星
+	 * @description：查询是否会员
+	 * 2017年3月17日
+	 */
+	public Complain selectMember(Complain complain) {
+		return dao.selectMember(complain);
+		
+	}
+
+	/**
+	 * @author：星星
+	 * @description：查询未/已处理的问题list
+	 * 2017年3月17日
+	 */
+	public List<Complain> selectStatus(Complain complain) {
+		return dao.selectStatus(complain);
+	}
+	
+	/**
+	 * @author：星星
+	 * @description：改变问题的状态
+	 * 2017年3月17日
+	 */
+	@Transactional(readOnly = false)
+	public void creatResult(Complain complain) {
+        dao.creatResult(complain); 		
+	}
+	
+	/**
+	 * 
+	 * @author：星星
+	 * @description：获取需要改变问题来源的处理过程Id
+	 * 2017年3月23日
+	 */
+	public Complain selectId(Complain complain) {
+		return dao.selectId(complain);		
+	}
+
+	public List<User> findUser(User user) {
+		return dao.findUser(user);
+	}
+
+	/**
+	 * @author：星星
+	 * @description：根据电话号码查询会员信息
+	 * 2017年3月10日
+	 */
+	public Complain getUser(String mobile) {		
+		return dao.getUser(mobile);
+	}
+
+	/**
+	 * @author：星星
+	 * @description：验证转交人
+	 * 2017年3月10日
+	 */
+	public List<Complain> getRedirectUserId(Complain complain) {
+		return dao.getRedirectUserId(complain);
+	}
+	
+	/**
+	 * @author：星星
+	 * @description：已/未处理/快速来电个自总记录数
+	 * 2017年3月10日
+	 */
+	public int getCount(Complain complain) {	
+		return dao.getCount(complain);
+	}
+	
+	/**
+	 * @author：星星
+	 * @description：根据电话查询是否有投诉记录
+	 * 2017年3月10日
+	 */
+	public List<Complain> selectSeek(Complain complain) {
+		return dao.selectSeek(complain);
+	}
+
+		
+}
+
