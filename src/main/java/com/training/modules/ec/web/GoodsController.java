db801e16afe857762847587e69014895bd468dbb
郭凯飞 <guokaifei@idengyun.com>
2017-04-14 17:19:45 +0800

    1.4.0

--- src/main/java/com/training/modules/ec/web/GoodsController.java
+++ src/main/java/com/training/modules/ec/web/GoodsController.java
@@ -1145,7 +1145,7 @@ public class GoodsController extends BaseController{
 	 */
 	@ResponseBody
 	@RequestMapping(value = "treeGoodsData")
-	public List<Map<String, Object>> treeGoodsData(@RequestParam(required=false) String extId,String franchiseeId,String goodsCategory,String actionType,String goodsName,String actionId,String isReal,String isOnSale,HttpServletResponse response) {
+	public List<Map<String, Object>> treeGoodsData(@RequestParam(required=false) String extId,String franchiseeId,String goodsCategory,String actionType,String goodsName,String actionId,String isReal,String isOnSale,String isAppshow,HttpServletResponse response) {
 		List<Map<String, Object>> mapList = Lists.newArrayList();
 		Goods goods=new Goods();
 		goods.setGoodsCategoryId(goodsCategory);
@@ -1154,6 +1154,7 @@ public class GoodsController extends BaseController{
 		goods.setFranchiseeId(franchiseeId);
 		goods.setIsReal(isReal);
 		goods.setIsOnSale(isOnSale);
+		goods.setIsAppshow(isAppshow);
 		if(actionId!=null){
 			goods.setActionId(Integer.parseInt(actionId));
 		}
