1d1cefed86020736517da14ce343b6a4696ae405
田胜 <tiansheng@idengyun.com>
2017-09-07 10:36:29 +0800

    代码提交错误,重新提交

--- src/main/java/com/training/modules/sys/entity/Office.java
+++ src/main/java/com/training/modules/sys/entity/Office.java
@@ -48,8 +48,8 @@ public class Office extends TreeEntity<Office> {
 	
 	private String isRecommend;            //是否推荐（0：未推荐；1：推荐）
 	private String isCargo;				//是否可售后(0:是；1否)
-	private String longitude;            //经度
-	private String latitude;            //纬度
+	private Double longitude;            //经度
+	private Double latitude;            //纬度
 	
 	private String 	iconUrl;		//机构icon
 	
@@ -282,19 +282,19 @@ public class Office extends TreeEntity<Office> {
 		this.isCargo = isCargo;
 	}
 
-	public String getLongitude() {
+	public Double getLongitude() {
 		return longitude;
 	}
 
-	public void setLongitude(String longitude) {
+	public void setLongitude(Double longitude) {
 		this.longitude = longitude;
 	}
 
-	public String getLatitude() {
+	public Double getLatitude() {
 		return latitude;
 	}
 
-	public void setLatitude(String latitude) {
+	public void setLatitude(Double latitude) {
 		this.latitude = latitude;
 	}
 
