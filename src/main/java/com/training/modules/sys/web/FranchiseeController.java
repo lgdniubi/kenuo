9e0e103bed01e065e8d38628a4fe19acfc3065a3
兵子 <chenbing@idengyun.com>
2017-11-28 11:22:21 +0800

    删除商家时同步数据

--- src/main/java/com/training/modules/sys/web/FranchiseeController.java
+++ src/main/java/com/training/modules/sys/web/FranchiseeController.java
@@ -7,7 +7,6 @@ import java.util.Map;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import org.apache.http.HttpRequest;
 import org.apache.shiro.authz.annotation.Logical;
 import org.apache.shiro.authz.annotation.RequiresPermissions;
 import org.springframework.beans.factory.annotation.Autowired;
