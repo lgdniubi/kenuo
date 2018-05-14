c12c79018580def8f62c87286da5cf24971266b0
wangfenglei <wangfenglei@idengyun.com>
2018-05-14 14:09:32 +0800

    技能设备恢复以前

--- src/main/java/com/training/modules/ec/service/SpecEquipmentService.java
+++ src/main/java/com/training/modules/ec/service/SpecEquipmentService.java
@@ -72,9 +72,9 @@ public class SpecEquipmentService extends CrudService<SpecEquipmentDao, Equipmen
 			specEquipmentDao.updateSpecEquipment(equipment);
 		}else{
 			//删除与新增设备名称相同的数据，新增的时候设置office_id为1（平台的id）
-			specEquipmentDao.deleteSpecEquipmentByName(equipment);
+//			specEquipmentDao.deleteSpecEquipmentByName(equipment);
 			//获取当前用户的office的id
-			equipment.setOfficeId("1");
+			equipment.setOfficeId(user.getOffice().getId());
 			equipment.setCreateBy(user);
 			specEquipmentDao.insertSpecEquipment(equipment);
 		}
