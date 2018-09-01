e232a2aeb0e178b5a9b6d24c784ade54ea0b1bbf
wangfenglei <wangfenglei@idengyun.com>
2018-09-01 10:00:25 +0800

    审核二次不推送审核消息

--- src/main/java/com/training/modules/sys/web/FranchiseeController.java
+++ src/main/java/com/training/modules/sys/web/FranchiseeController.java
@@ -358,12 +358,12 @@ public class FranchiseeController extends BaseController{
 					ModelFranchisee modelSelect = userCheckService.getModelFranchiseeByUserid(modelFranchisee.getUserid());
 					userCheckService.saveModelFranchisee(modelFranchisee);//保存手艺人权益信息
 					userCheckService.pushMsg(modelFranchisee,modelSelect,opflag);//重新授权成功发送消息
-					userCheckService.pushMsg(userCheck, "您已具备手艺人用户的权益，开启新旅程吧。");//授权成功发送消息
+//					userCheckService.pushMsg(userCheck, "您已具备手艺人用户的权益，开启新旅程吧。");//授权成功发送消息
 				}else if ("qy".equals(opflag)){
 					ModelFranchisee modelSelect = userCheckService.getQYModelFranchiseeByUserid(modelFranchisee.getUserid());
 					userCheckService.saveQYModelFranchisee(modelFranchisee,find);//保存企业权益信息
 					userCheckService.pushMsg(modelFranchisee,modelSelect,opflag);//重新授权成功发送消息
-					userCheckService.pushMsg(userCheck, "您已具备企业用户的权益，开启新旅程吧。");//授权成功发送消息
+//					userCheckService.pushMsg(userCheck, "您已具备企业用户的权益，开启新旅程吧。");//授权成功发送消息
 				}
 				redisClientTemplate.del("UTOKEN_"+modelFranchisee.getUserid());
 				addMessage(redirectAttributes, "成功");
