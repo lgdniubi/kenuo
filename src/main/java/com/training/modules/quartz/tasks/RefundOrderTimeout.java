3d7c0cb1374c423d60cbf31e70feabb804db7a15
wangfenglei <wangfenglei@idengyun.com>
2018-10-12 14:17:50 +0800

    账单超时定时器

--- src/main/java/com/training/modules/quartz/tasks/RefundOrderTimeout.java
+++ src/main/java/com/training/modules/quartz/tasks/RefundOrderTimeout.java
@@ -73,8 +73,8 @@ public class RefundOrderTimeout extends CommonService{
 			
 			String timeout = ParametersFactory.getTrainsParamValues("refundOrderTimeout");
 //			timeout = timeout.equals("-1")?"120":timeout;
-			refundOrderService.refundOrderTimeout(timeout);
 			refundOrderService.refundOrderTimeoutUpdateOfficeState(timeout);//改机构--对账单的状态
+			refundOrderService.refundOrderTimeout(timeout);
 			
 		
 		}catch (Exception e) {
